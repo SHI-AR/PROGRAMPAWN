6c3b668bf6706d3e40a313d86d6146dc112fd7b6
SHI-AR <COMPUTE.SGAZETTEDRAGONFLY@OUTLOOK.COM>
2016-08-28 10:14:01 -0500

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -69,7 +69,7 @@
             this.pictureBox1.Image = global::WindowsFormsApplication1.Properties.Resources.CALENDAR;
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(740, 613);
+            this.pictureBox1.Size = new System.Drawing.Size(1186, 767);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -78,7 +78,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(764, 637);
+            this.ClientSize = new System.Drawing.Size(1210, 791);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
